3055c50ce6fa81c0b2d5f6f54a2fc989ed9fa2bc
iaihabjb <ihab@ihab-baghdadi.com>
2020-03-23 12:26:20 -0400

    update

--- src/candlestick/candlestick.go
+++ src/candlestick/candlestick.go
@@ -4,19 +4,23 @@ import (
   "fmt"
   "math/rand"
   "time"
+  "mylib/asker"
+  "strconv"
 )
 
 type Player struct {
   deck []int
   hand []int
   score int
+  isHuman bool
 }
 
-func NewPlayer() *Player {
+func NewPlayer(isH bool) *Player {
   res := &Player{
     makeDeck(),
     make([]int, 0),
     0,
+    isH,
   }
 
   res.DrawCards(3)
@@ -25,9 +29,47 @@ func NewPlayer() *Player {
 
 
 
-func (self *Player) DrawCards(n int) {
+func (self *Player) DrawCards(n int) bool {
+  if len(self.deck) == 0 {
+    return false
+  }
   self.hand = append(self.hand, self.deck[:n]...)
   self.deck = self.deck[n:]
+  return true
+}
+
+func (self *Player) ChooseCard() int {
+  a := self.hand[0]
+  if self.isHuman {
+    a = AskCard(self.hand)
+  }
+
+  h2 := make([]int, 0)
+  for _, v := range self.hand {
+    if v != a {
+      h2 = append(h2, v)
+    }
+  }
+  self.hand = h2
+  self.DrawCards(1)
+  return a
+}
+
+func AskCard(cards []int) int {
+  mess := "Please choose a card from "
+  for _, v := range cards {
+    mess += strconv.Itoa(v) + ", "
+  }
+  for {
+    a := asker.AskInt(mess)
+    if contains(cards, a) > 0 {
+      return a
+    }
+    fmt.Println("Stick to the list please")
+  }
+
+
+
 }
 
 // contains returns the number of elements in the slice that match the comparator (c)
@@ -115,17 +157,48 @@ func makeDeck()[]int{
   return cards
 }
 
+func round(pl []*Player, first int) int {
+  pnum := first
+  complete := false
+  chosens := make([]int, len(pl))
+  for !complete {
+    chosens[pnum] = pl[pnum].ChooseCard()
+    fmt.Printf("p%d : %d\n", pnum, chosens[pnum])
+    pnum = (pnum + 1) % len(pl)
+    if pnum == first {
+      complete = true
+    }
+  }
+
+  winP, winC := DecideTrick(chosens)
+  pl[winP].score++
+  fmt.Printf("Player %d wins with %d"\n", winP, winC)
+
+  scoresMess := "Scores : "
+  for _, v := range pl {
+    scoresMess += strconv.Itoa(v.score) + " "
+  }
+  fmt.Println(scoresMess)
+
+  return winP
+}
+
 func main() {
-
-
   rand.Seed(time.Now().Unix())
-
   players := make([]*Player, 4)
 
   for i := 0; i < 4; i++ {
-    players[i] = NewPlayer()
-    fmt.Printf("%s, \n%s\n, %d\n\n", players[i].deck, players[i].hand, players[i].score)
+    players[i] = NewPlayer(i == 0)
+    // fmt.Printf("%s, \n%s\n, %d\n\n", players[i].deck, players[i].hand, players[i].score)
   }
 
-  fmt.Println(players)
+  w := -1
+  for i := 0; i < 13; i ++ {
+    if w == -1 {
+      w = rand.Intn(4)
+    }
+    w = round(players, w)
+  }
+
+  // Get winner
 }
